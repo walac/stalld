walac/stalld

commit 5bd4be43bc5bdcbbbb9bfb84309ae6fe6965e5ad

Makefile: Remove redhat-specific checks
Also, remove unused targets and $(SOPTS).

Signed-off-by: Derek Barbosa <debarbos@redhat.com>
Signed-off-by: Clark Williams <williams@redhat.com>

Makefile
@@ -62,26 +62,9 @@ MOPTS   :=  	$(strip $(MTUNE)) $(strip $(M64)) -mno-omit-leaf-frame-pointer
 
 WOPTS	:= 	-Wall -Werror=format-security
 
-SOPTS	:= 	-specs=/usr/lib/rpm/redhat/redhat-hardened-cc1 -specs=/usr/lib/rpm/redhat/redhat-annobin-cc1
-
 DEFS	:=	-DUSE_BPF=$(USE_BPF) -D_FORTIFY_SOURCE=3 -D_GLIBCXX_ASSERTIONS -DDEBUG_STALLD=$(DEBUG)
 
-
-# note that RPMCFLAGS and RPMLDFLAGS are variables that come from the specfile when
-# building for Fedora/CentOS/RHEL/et al
-#
-
-ifeq ($(RPMCFLAGS),)
-CFLAGS	:=	-DVERSION=\"$(VERSION)\" $(FOPTS) $(MOPTS) $(WOPTS) $(SOPTS) $(DEFS)
-else
-CFLAGS	:=	 $(RPMCFLAGS) $(DEFS)
-endif
-
-ifeq ($(IS_LEGACY),true)
-$(info Compiling with Legacy Mode enabled)
-$(info Overwriting RPMCFLAGS...)
-CFLAGS	:=	-DVERSION=\"$(VERSION)\" $(FOPTS) $(MOPTS) $(WOPTS) $(DEFS) -std=c99 -DLEGACY
-endif
+CFLAGS	:=      -DVERSION=\"$(VERSION)\" $(FOPTS) $(MOPTS) $(WOPTS) $(DEFS)
 
 ifeq ($(DEBUG),0)
 CFLAGS	+=	-g -O2
@@ -90,11 +73,7 @@ CFLAGS	+=	-g3
 endif
 $(info CFLAGS=$(CFLAGS))
 
-ifeq ($(RPMLDFLAGS),)
 LDFLAGS	:=	-ggdb -znow -pie
-else
-LDFLAGS	:=	$(RPMLDFLAGS) -ggdb -znow -pie
-endif
 $(info LDFLAGS=$(LDFLAGS))
 
 LIBS	:=	 -lpthread
@@ -219,7 +198,7 @@ uninstall:
 	rm -f $(DESTDIR)$(BINDIR)/throttlectl
 	make -C systemd DESTDIR=$(INSPATH) uninstall
 
-.PHONY: clean tarball systemd push annocheck help
+.PHONY: clean tarball
 clean:
 	@test ! -f stalld || rm stalld
 	@test ! -f stalld-static || rm stalld-static
@@ -237,9 +216,6 @@ tarball:  clean
 	cp -r $(DIRS) $(FILES) $(NAME)-$(VERSION)
 	tar $(TAROPTS) --exclude='*~' $(NAME)-$(VERSION)
 	rm -rf $(NAME)-$(VERSION)
-
-annocheck: stalld
-	annocheck --ignore-unknown --verbose --profile=el10 --debug-dir=/usr/lib/debug/ ./stalld
 
 help:
 	@echo  'Cleaning targets:'

commit 38fbda424a839af13a2b3633b94cf2498686be69

Makefile: Remove unnecessary -m64 compiler flag
Remove the M64 variable and all related architecture-specific overrides
from the Makefile. The -m64 flag is unnecessary because GCC defaults to
generating code for the native architecture. On 64-bit systems it
produces 64-bit binaries automatically, and on 32-bit systems the flag
causes build failures.

This change fixes builds on i686 and 32-bit powerpc architectures,
which were broken after commit 5bd4be43bc5b ("Makefile: Remove
redhat-specific checks")removed the RPMCFLAGS conditional that previously
allowed RPM builds to override the internal compiler flags. Rather than
adding M64 overrides for each 32-bit architecture, removing the flag
entirely is the cleaner solution since it provides no benefit on any platform.

Signed-off-by: Wander Lairson Costa <wander@redhat.com>

Makefile
@@ -16,11 +16,7 @@ $(info GCC_VER=$(GCC_VER))
 
 USE_BPF := 1
 MTUNE	:= -mtune=generic
-M64	:= -m64
 
-ifeq ($(ARCH),aarch64)
-M64	:=
-endif
 ifeq ($(ARCH),i686)
 USE_BPF := 0
 endif
@@ -37,7 +33,6 @@ MTUNE := -mtune=powerpc
 endif
 ifeq ($(ARCH),riscv64)
 FCF_PROTECTION := "-fcf-protection=none"
-M64	:=
 MTUNE := -mtune=generic-ooo
 endif
 
@@ -63,7 +58,7 @@ OMIT_LEAF_FP := $(shell echo 'int main(void){return 0;}' | \
 		-o /dev/null 2>/dev/null && \
 		echo '-mno-omit-leaf-frame-pointer')
 
-MOPTS   :=  	$(strip $(MTUNE)) $(strip $(M64)) $(strip $(OMIT_LEAF_FP))
+MOPTS   :=  	$(strip $(MTUNE)) $(strip $(OMIT_LEAF_FP))
 
 WOPTS	:= 	-Wall -Werror=format-security
 